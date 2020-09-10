cffaee6252b1d0b962e98b1899bec88725e7b875
sonph <sonph@smartosc.com>
2020-09-10 09:03:57 +0700

    Second Commits

--- find_numbers.php
+++ find_numbers.php
@@ -1,33 +1,27 @@
 <?php
-
+//get data in file number.txt
 $myfile=file_get_contents('number.txt');
 $mangtrong=explode("\n",$myfile);
 
-
+//create containing results
 $result=[];
+//create 2 loops
 for($i=0;$i<count($mangtrong);$i++){
+    //create a counter
     $coun=0;
-
-    $num=$mangtrong[$i];
     for($j=0;$j<count($mangtrong);$j++) {
-        if($num==$mangtrong[$j]){
+        if($mangtrong[$i]==$mangtrong[$j]){
             $coun=$coun+1;
 
         }
         if ($coun>=3){
-            if (!in_array($num,$result)){
-            array_push($result,$num);
+            if (!in_array($mangtrong[$i],$result)){
+                //add number >=3 into array result
+            array_push($result,$mangtrong[$i]);
             }
-    break;
+        break;
         }
-
-
-
-
-
     }
-
-
 }
 print_r($result);
 
--- result.php
+++ result.php
@@ -1,14 +1,16 @@
 
 <?php
-
-
-    //$bk=$_POST['bk'];
+//$bk=$_POST['bk'];
+//Enter vairable
 $bk=$argv[1];
+//Consider non-negative conditions
     if ($bk<0){
          echo "Không nhập số âm"."\n";
          return;
     }
+    //consider the condition as number
     if(is_numeric($bk)){
+    //Recipe
     $dientich=pi()*$bk*$bk;
     $bankinh=2*pi()*$bk;
     echo ("Diện tích là: ".round($dientich,2)."\n");
